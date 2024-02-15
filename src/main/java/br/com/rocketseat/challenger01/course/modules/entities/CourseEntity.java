fbd7c1423b8bdbd6db5296487f0b62e125af2b10
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2024-02-15 11:13:10 -0300

    feat: implement Course Entity

---
+++ src/main/java/br/com/rocketseat/challenger01/course/modules/entities/CourseEntity.java
@@ -0,0 +1,36 @@
+package br.com.rocketseat.challenger01.course.modules.entities;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import br.com.rocketseat.challenger01.course.modules.enums.Active;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "courses")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class CourseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  private String name;
+
+  private Active active;
+
+  @Column(name = "created_at")
+  private LocalDateTime createdAt;
+
+  @Column(name = "updated_at")
+  private LocalDateTime updatedAt;
+
+}
