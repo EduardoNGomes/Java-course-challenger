3bae4557d04015d757707a3204605f8e6f6f1ea3
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2024-02-15 13:09:17 -0300

    feat: implement Create Course DTO

---
+++ src/main/java/br/com/rocketseat/challenger01/course/modules/dto/CreateCourseDTO.java
@@ -0,0 +1,12 @@
+package br.com.rocketseat.challenger01.course.modules.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateCourseDTO {
+  private String name;
+}
